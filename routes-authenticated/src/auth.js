9eb5958aa6913551779e14a74660bcba876ad4d4
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2020-01-30 12:35:38 -0300

    Rota com Autentificação - Construção basica de estrutura de uma rota privada

--- routes-authenticated/src/auth.js
+++ routes-authenticated/src/auth.js
@@ -2,5 +2,17 @@
     //aqui fica toda  logica de autenticação. 
     //Ex: Ir o localstorage verificar se o usuario possui o token(JWT) e etc.
 
+    /* EXEMPLO PRÁTICO
+
+    export const isAuthenticated = () =>  {
+      if (localStorage.getItem('TOKEN')) {
+          return true
+      } else {
+          return false
+      }
+    }
+    */
+
+
     return true
   };
--- routes-authenticated/src/routes.js
+++ routes-authenticated/src/routes.js
@@ -8,10 +8,23 @@ import React from 'react';
   'exact' na Route: esse cara defini que deve ser obrigatoriamente aquela
   rota definida(nem mais nem menos). ele funciona como um tratamento 'if' 
 */
-import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
+import { isAuthenticated } from './auth'
 
-const PrivateRoute = ({ component: Component }) => (
-  <Route />
+//padronizando o nome 'component' para 'Component'
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  // Mesma routes que estamos chamando la em baixo
+  <Route {...rest} render={props => (
+    //renderizando o component com as novas props
+    isAuthenticated() ? (
+      <Component { ...props } />
+    ) : (
+      /*caso ao contrario redirecionamos ele para outra rota que nao necessita Permission/Authentication
+      O state abaixo, nao deixa que o usuario perca o estado/historico da pagina, possibilitando que o usuario
+      consiga voltar ou avançar durante a navegação*/
+      <Redirect to={{ pathname: '/', state: { from: props.location } }}/>      
+    )
+  )} />
 )
 
 const Routes = () => (
@@ -19,6 +32,12 @@ const Routes = () => (
     <Switch>
       {/* definindo a primeira rota com o path, e ele ira chamar algum componet; */}
       <Route exact path='/' component={() => <h1>Hello World Routes</h1>} ></Route>
+      
+      {/* Definindo como Privado a rota '/app'  */}
+      <PrivateRoute  path='/app'component ={() => <h1>Você esta Autenticado</h1>}/>
+      
     </Switch>
   </BrowserRouter>
 );
+
+export default Routes;
